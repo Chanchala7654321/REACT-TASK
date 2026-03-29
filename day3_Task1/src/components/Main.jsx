ba3a6f2d96d25779abb0d1edcd3c724485a20094
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2026-03-29 20:57:19 +0530

    completed day 2 task 1

---
+++ day3_Task1/src/components/Main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ImgOne from './ImgOne'
+import ImgTwo from './ImgTwo'
+import ImgThree from './ImgThree'
+import ImgFive from './ImgFive'
+import ImgSix from './ImgSix'
+import ImgSeven from './ImgSeven'
+import ImgEight from './ImgEight'
+import ImgNine from './ImgNine'
+import ImgTen from './ImgTen'
+import ImgEleven from './ImgEleven'
+import ImgTwelve from './ImgTwelve'
+
+import './Main.css'
+
+function Main() {
+    return (
+        <div className="center">
+
+            <ImgOne />
+            <ImgTwo />
+            <ImgThree />
+            <ImgFive />
+            <ImgFive />
+            <ImgSix />
+            <ImgSeven />
+            <ImgEight />
+            <ImgNine />
+            <ImgTen />
+            <ImgEleven />
+            <ImgTwelve />
+        </div>
+
+    )
+}
+
+export default Main
