a3013f3bf45162eef6719ee34540b465a1f8cd0f
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2026-03-29 20:52:19 +0530

    Update component

--- day2/src/App.jsx
+++ day2/src/App.jsx
@@ -1,32 +1,18 @@
 
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <header className='header'>
-        <h1>MY REACT APP</h1>
-      </header>
-      <main className='content'>
-        <section className='hero'>
-          <h2>Welcome to My React App</h2>
-          <p>This is a simple React application to demonstrate the structure of a React component.</p>
-          <p>Features of React</p>
-        </section>
-        <section className='features'>
-          <p>
-            <ol>
-              <li>Component-Based: React allows you to build encapsulated components that manage their own state, then compose them to make complex UIs.</li>
-              <li>Declarative: React makes it easy to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.</li>
-              <li>Learn Once, Write Anywhere: You can develop new features in React without rewriting existing code. React can also render on the server using Node and power mobile apps using React Native.</li>
-            </ol>
-          </p>
-        </section>
-      </main>
-      <footer className='footer'>
-        <p>© 2024 My React App. All rights reserved.</p>
-      </footer>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
