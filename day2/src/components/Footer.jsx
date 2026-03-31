ea976547438ec1d67600bc52ab4bf79d02b4f10d
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2026-03-31 14:47:02 +0530

    Update day2

---
+++ day2/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <p>© 2024 My React App. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer
